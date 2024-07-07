edb7e768a0cdb9b2fd37a4d5c12427e60f5ae294
TUN <galaxytuntun14@gmail.com>
2024-07-07 15:50:55 +0700

    MAIN

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import tun from "./assets/tun.jpg";
+
+function App() {
+  return (
+    <div>
+      <h1>PUSS_E</h1>
+      <img src={tun}></img>
+      <h1>K-Y</h1>
+    </div>
+  );
+}
+
+export default App;
